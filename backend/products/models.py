c505ee9ea19c44745fbe0fbfc66b7372c95cea7b
ibrohim2911 <77937292+ibrohim2911@users.noreply.github.com>
2023-03-10 21:27:47 +0500

    ydtf

--- backend/products/models.py
+++ backend/products/models.py
@@ -10,11 +10,15 @@ class createSellers(models.Model):
     
 
 
-
+types=('Bosh kiyimlar','Bosh kiyimlar'),("ko'ylaklar","ko'ylaklar"),("Etiklar va qo'nji kalta etiklar","Etiklar va qo'nji kalta etiklar"),("Shimlar va jinsilar","Shimlar va jinsilar"), ('Tufli va mokasinlar','Tufli va mokasinlar'),('Futbolkalar va maykalar','Futbolkalar va maykalar'),('Sport kiyimlari','Sport kiyimlari'),('Jemperlar va sviterlar','Jemperlar va sviterlar'),('Ustki kiyimlar','Ustki kiyimlar'),("Kostyumlar va to'plamlar","Kostyumlar va to'plamlar"),('Pidjaklar va jiletlari','Pidjaklar va jiletlari'),('Krossovkalar va kedalar',' Krossovkalar va kedalar')
+# offers=('hech qaysi','none'),('5%','5%'),('10%','10%'),('15%','15%'),('20%','20%'),('30%','30%'),('40%','40%'),('50%','50%'),
 class CreateProduct(models.Model):
     name=models.CharField(max_length=400)
     price=models.CharField(max_length=100)
     full_detail=models.TextField()
+    # offer=models.CharField(max_length=12, choices=offers, default=None)
     images=models.ImageField(upload_to ='uploads/')
+    producttype=models.CharField(choices=types, max_length=200, default='nomalum')
+    seller=models.ForeignKey(createSellers,on_delete=models.CASCADE, default=5)
     def __str__(self):
         return self.name
